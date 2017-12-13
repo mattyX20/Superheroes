4915066628f11d99af2700cc85b125c62cc83c52
Matteo Altobelli <altobelli.matteo45@gmail.com>
2017-12-13 22:32:36 +0100

    New Version
    Questa versione contiene un aggiornamento con prima versione del layout

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,15 @@
-// chiamata generale di tutti i personaggi
-$.ajax({
-        method: "GET",
-        url: "src/api/characters.json",
-        data: {
-            "apikey": "6855e608549da5f59a51c62c2f4f57b3"
-        }
-    })
-    .done(function (msg) {
-        console.log(msg);
+$(function () {
+    // chiamata generale di tutti i personaggi
+    $.getJSON("src/api/characters.json", {
+        "apikey": "6855e608549da5f59a51c62c2f4f57b3"
+    }).done(function (msg) {
+        console.log(arguments);
+        var source = document.getElementById("character-template").innerHTML;
+        var template = Handlebars.compile(source);
+        var context = msg.data;
+        var html = template(context);
+        $("#characters-container").html(html);
+    }).fail(function (err) {
+        console.log(err);
     });
+});
