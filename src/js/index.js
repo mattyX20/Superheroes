a9416ceedccca45c47a12d5bc227733c52293143
mattyX20 <altobelli.matteo45@gmail.com>
2018-01-15 17:35:44 +0100

    Develop system ok
    Github fa i macelli addesso sistemati

--- src/js/index.js
+++ src/js/index.js
@@ -11,39 +11,21 @@ $(function () {
         var html = template(context);
         // Mostra il template nel html
         $("#characters-container").html(html);
-<<<<<<< HEAD:src/js/index.js
         $(".character-container").flip({
             trigger: 'manual'
         });
-=======
-        $(".character-container").flip({trigger:'manual'});
->>>>>>> origin/Develop:page/src/js/index.js
         // recupero ID
         $(".character-container")
             // Abilita il flip
             .on("click", function () {
-<<<<<<< HEAD:src/js/index.js
                 $(this).flip("true");
                 // Mostra nome e descrizione dei personaggi  flippando
             }).on("mouseover", function () {
                 $(this).find(".front .character-name").fadeIn();
                 // Nasconde descrizione dei personaggi unflippando uscendo fuori con il mouse
-=======
-                $(this).flip("toggle");
-                // Nasconde descrizione dei personaggi
-            }).on("mouseover", function () {
-                $(this).find(".front .character-name").fadeIn();
->>>>>>> origin/Develop:page/src/js/index.js
             }).on("mouseleave", function () {
                 $(this).find(".front .character-name").fadeOut();
                 $(this).flip(false);
             });
-<<<<<<< HEAD:src/js/index.js
     })
 });
-=======
-    }).fail(function (err) {
-        console.log(err);
-    });
-});
->>>>>>> origin/Develop:page/src/js/index.js
